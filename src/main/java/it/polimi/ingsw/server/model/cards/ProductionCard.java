16caf751b7a95375b668587724f8732cda436cca
Matteo <61700022+Beltrante@users.noreply.github.com>
2021-04-02 16:52:10 +0200

    modified xml groupID and ProductionCard Interface

--- src/main/java/it/polimi/ingsw/server/model/cards/ProductionCard.java
+++ src/main/java/it/polimi/ingsw/server/model/cards/ProductionCard.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.server.model.cards;
 
+import it.polimi.ingsw.server.model.resources.Resource;
+
+import java.util.List;
+
 public interface ProductionCard {
+    List<Resource> usePower();
+    List<Resource> getInResources();
+    List<Resource> getOutResources();
+    boolean canDoProduction(List<Resource> resources);
 }
