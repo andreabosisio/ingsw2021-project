cb12c316248f227a9051c11304679076f2ae462c
Matteo <61700022+Beltrante@users.noreply.github.com>
2021-05-19 15:50:31 +0200

    implemented first part of cli event deSerializations and handling also implemented startOfTurnNotifications and some failed event resending

--- src/main/java/it/polimi/ingsw/client/events/send/ProductionActionEvent.java
+++ src/main/java/it/polimi/ingsw/client/events/send/ProductionActionEvent.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw.client.events.send;
 import java.util.List;
 import java.util.Map;
 
-public class ProductionSendEvent extends SendEvent{
+public class ProductionActionEvent extends SendEvent{
     private final Map<Integer, List<Integer>> inResourcesForEachProductions;
     private final Map<Integer, String> outResourcesForEachProductions;
-    public ProductionSendEvent(Map<Integer, List<Integer>> inResourcesForEachProductions,Map<Integer, String> outResourcesForEachProductions) {
+    public ProductionActionEvent(Map<Integer, List<Integer>> inResourcesForEachProductions, Map<Integer, String> outResourcesForEachProductions) {
         super("productionAction");
         this.inResourcesForEachProductions = inResourcesForEachProductions;
         this.outResourcesForEachProductions = outResourcesForEachProductions;
