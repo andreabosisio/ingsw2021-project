557a8e09dc6062e4d6d71c8bae5dff6778baf1a2
Matteo Beltrante <10575137@polimi.it>
2021-05-28 18:37:19 +0200

    fixed bug in popup for DevGridPopupController

--- src/main/java/it/polimi/ingsw/client/view/gui/controllers/DevGridPopupController.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/DevGridPopupController.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.client.view.gui.controllers;
 
 import it.polimi.ingsw.client.view.gui.GraphicUtilities;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 
 public class DevGridPopupController {
@@ -9,6 +10,13 @@ public class DevGridPopupController {
     GridPane devGrid;
     @FXML
     private void initialize() {
+        //todo disable or do nothing?
+        /*
+        for(Node n:devGrid.getChildren()){
+            n.setDisable(true);
+        }
+
+         */
         GraphicUtilities.populateDevGrid(devGrid);
     }
 
