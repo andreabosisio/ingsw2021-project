fe53d6080c5c5d7c984a79d9f5e5c781ae286494
Matteo Beltrante <10575137@polimi.it>
2021-06-29 12:10:18 +0200

    fixed a bug in persistence where a crash in setup wouldn't work

---
+++ src/main/java/it/polimi/ingsw/server/events/receive/CheatEvent.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.server.events.receive;
+
+import it.polimi.ingsw.exceptions.*;
+import it.polimi.ingsw.server.model.ModelInterface;
+
+public class CheatEvent extends  ReceiveEvent{
+    private static final String CHEAT_TYPE = "cheat";
+    public CheatEvent(String sender) {
+        super(sender,CHEAT_TYPE);
+    }
+
+    @Override
+    public boolean doAction(ModelInterface modelInterface) throws InvalidIndexException, InvalidEventException, NonStorableResourceException, EmptySlotException, NonAccessibleSlotException, InvalidSetupException {
+        modelInterface.cheat();
+        return true;
+    }
+}
