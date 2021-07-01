c2e8a0969ca27dca37a1e8e69b4cd30ae3163783
Matteo Beltrante <10575137@polimi.it>
2021-07-01 20:47:08 +0200

    broken refactor

--- src/main/java/it/polimi/ingsw/server/utils/EventsFromClientObserver.java
+++ src/main/java/it/polimi/ingsw/server/utils/EventsFromClientObserver.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.server.utils;
 
 import it.polimi.ingsw.server.events.receive.EventFromClient;
 
-public interface EventsForClientObserver {
+public interface EventsFromClientObserver {
     /**
      * This method is called by a Virtual View to notify the observers that an Event from the Client has been received.
      *
