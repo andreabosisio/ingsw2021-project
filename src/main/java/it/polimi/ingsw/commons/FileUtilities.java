e51b3c5cb47568e42af24840bf0747d004c5e0d0
andreabosisio <andrea2.bosisio@mail.polimi.it>
2021-07-02 21:12:35 +0200

    read with getResourcesAsStream

--- src/main/java/it/polimi/ingsw/commons/FileUtilities.java
+++ src/main/java/it/polimi/ingsw/commons/FileUtilities.java
@@ -4,21 +4,22 @@ import com.google.gson.*;
 
 import java.io.*;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * This class contains the path of the various Json File and the necessary method to work with these one
  */
 public abstract class FileUtilities {
     private static final Gson gson = new Gson();
-    public static final String SAVED_GAME_PATH = "src/main/resources/gameSaved.json";
-    public static final String MARKET_DEFAULT_CONFIG_PATH = "src/main/resources/defaultMarketConfig.json";
-    public static final String SAVED_MARKET_DATA_PATH = "src/main/resources/initialMarketState.json";
-    public static final String SAVED_DEV_CARD_DATA_PATH = "src/main/resources/initialGridState.json";
-    public static final String SAVED_LEADER_CARD_DATA_PATH = "src/main/resources/initialDeckLeaderState.json";
-    public static final String UNMODIFIABLE_DEVELOPMENT_CARDS_PATH = "src/main/resources/developmentCards.json";
-    public static final String UNMODIFIABLE_LEADER_CARDS_PATH = "src/main/resources/leaderCards.json";
-    public static final String UNMODIFIABLE_SOLO_TOKEN_PATH = "src/main/resources/soloActionTokens.json";
-    public static final String SOLO_SAVED_TOKEN_PATH = "src/main/resources/savedActionTokens.json";
+    public static final String SAVED_GAME_PATH = "/gameSaved.json";
+    public static final String MARKET_DEFAULT_CONFIG_PATH = "/defaultMarketConfig.json";
+    public static final String SAVED_MARKET_DATA_PATH = "/initialMarketState.json";
+    public static final String SAVED_DEV_CARD_DATA_PATH = "/initialGridState.json";
+    public static final String SAVED_LEADER_CARD_DATA_PATH = "/initialDeckLeaderState.json";
+    public static final String UNMODIFIABLE_DEVELOPMENT_CARDS_PATH = "/developmentCards.json";
+    public static final String UNMODIFIABLE_LEADER_CARDS_PATH = "/leaderCards.json";
+    public static final String UNMODIFIABLE_SOLO_TOKEN_PATH = "/soloActionTokens.json";
+    public static final String SOLO_SAVED_TOKEN_PATH = "/savedActionTokens.json";
 
     /**
      * Reset the Game Data saved in the SaveGame file.
@@ -45,12 +46,8 @@ public abstract class FileUtilities {
      * @return the JsonElement of the parsed file
      */
     public static JsonElement getJsonElementFromFile(String path) {
-        File input = new File(path);
         try {
-            return JsonParser.parseReader(new FileReader(input));
-        } catch (FileNotFoundException e) {
-            System.err.println("file not found");
-            e.printStackTrace();
+            return JsonParser.parseReader(new InputStreamReader(Objects.requireNonNull(FileUtilities.class.getResourceAsStream(path))));
         } catch (Exception e) {
             System.err.println("error in the json file format");
             e.printStackTrace();
